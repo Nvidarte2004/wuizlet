6e5a9e26e82288d23f3993e52c15bebc46d04b21
Nvidarte2004 <nvidarte2004@gmail.com>
2025-05-16 14:15:53 -0700

    Prep for render deployment

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# settings.py
+
+import os
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# For collectstatic command
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
